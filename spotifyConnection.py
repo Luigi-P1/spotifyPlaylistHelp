1c3f46156828097a4f29bedbc55120689d7c38f3
unknown <lpuma@spartaglobal.com>
2022-10-03 17:06:40 +0100

    making it easier for new user

--- spotifyConnection.py
+++ spotifyConnection.py
@@ -8,12 +8,13 @@ def connect_to_spotify():
     CLIENT_ID = hiddenVariables.CLIENT_ID
     CLIENT_SECRET = hiddenVariables.CLIENT_SECRET
     SPOTIFY_REDIRECT_URI = hiddenVariables.SPOTIFY_REDIRECT_URI
-    spotify_scope = hiddenVariables.spotify_scope
+    spotify_scope = hiddenVariables.SPOTIFY_SCOPE
+    user_name = hiddenVariables.USER_NAME
     SP = spotipy.Spotify(auth_manager=SpotifyOAuth(open_browser=False, scope=spotify_scope,
                                                    client_id=CLIENT_ID,
                                                    client_secret=CLIENT_SECRET,
                                                    redirect_uri=SPOTIFY_REDIRECT_URI,
-                                                   cache_handler=CacheFileHandler(username='Luigi.puma')))
+                                                   cache_handler=CacheFileHandler(username=user_name)))
 
     return SP
 
